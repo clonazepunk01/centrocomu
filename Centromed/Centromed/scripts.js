b7bf1ab2683d07789b6e242e47d43d7747428903
clonazepunk01 <150389766+clonazepunk01@users.noreply.github.com>
2024-09-03 14:51:07 -0400

    Add files via upload

---
+++ Centromed/Centromed/scripts.js
@@ -0,0 +1,21 @@
+let currentIndex = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = document.querySelectorAll('.slide').length;
+
+    currentIndex += direction;
+
+    if (currentIndex < 0) {
+        currentIndex = totalSlides - 1;
+    } else if (currentIndex >= totalSlides) {
+        currentIndex = 0;
+    }
+
+    const offset = -currentIndex * 100;
+    slides.style.transform = `translateX(${offset}%)`;
+}
+
+setInterval(() => {
+    moveSlide(1);
+}, 3000);
